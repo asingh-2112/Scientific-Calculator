4bb4e3d98ba70b2bd8e0ee2ad834ebe645fe14a6
Abhishek <aksinghrbj2112@gmail.com>
2025-03-01 15:56:14 +0530

    Added claculator project

---
+++ Scientific_Calculator/src/main/java/org/example/ScientificCalculator.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class ScientificCalculator {
+
+    public static double squareRoot(double number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Square root of a negative number is not defined.");
+        }
+        return Math.sqrt(number);
+    }
+
+
+    public long factorial(int x) {
+        if (x < 0) throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
+        long result = 1;
+        for (int i = 2; i <= x; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public double naturalLog(double x) {
+        if (x <= 0) throw new IllegalArgumentException("Natural logarithm is not defined for non-positive numbers.");
+        return Math.log(x);
+    }
+
+    public double power(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
+}
